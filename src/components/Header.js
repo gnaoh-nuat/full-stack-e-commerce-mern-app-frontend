e80515f9827259e3877898fc89b7aaf5165ef3e9
Hoàng Mạnh Tuấn <tino2deptrai@gmail.com>
2025-10-10 16:15:16 +0700

    commit 1

---
+++ src/components/Header.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Logo from "./Logo";
+import { GrSearch } from "react-icons/gr";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="h-16 shadow-md">
+      {/* Logo */}
+      <div className="h-full container mx-auto flex items-center px-4 justify-between">
+        <div className="">
+          <Logo w={90} h={50} />
+        </div>
+
+        {/* search bar */}
+        <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
+          <input
+            type="text"
+            placeholder="search product here..."
+            className="w-full outline-none"
+          />
+          <div className="text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white">
+            <GrSearch />
+          </div>
+        </div>
+
+        {/* user & cart & login log out*/}
+        <div className="flex items-center gap-7">
+          {/* user */}
+          <div className="text-3xl cursor-pointer">
+            <FaRegCircleUser />
+          </div>
+
+          {/* cart */}
+          <div className="text-3xl relative">
+            <span>
+              <FaShoppingCart />
+            </span>
+            <div className="bg-red-600 text-white flex items-center justify-center rounded-full w-5 h-5 p-1 absolute -top-2 -right-3">
+              <p className="text-sm">0</p>
+            </div>
+          </div>
+
+          {/* login & log out*/}
+          <div>
+            <button className="px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div class="text-red-950">Home</div>;
+};
+
+export default Home;
